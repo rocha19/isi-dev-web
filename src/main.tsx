f4c1f1608976936b81e9f961f0d36267381f1153
rocha19 <rochaarqueologia@outlook.com>
2025-06-25 23:02:22 -0300

    chore(app): update entrypoint and base HTML structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
-
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<RouterProvider router={router} />
-	</StrictMode>,
+	<QueryClientProvider client={queryClient}>
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>
+	</QueryClientProvider>,
 );
